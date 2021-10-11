7973cef7ca00c9bcc2c17d3db95dd0347cf37d7f
shamagir <shamagir@gmail.com>
2021-10-11 21:45:10 +0300

    fix: webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '/dist'),
+    static: path.resolve(__dirname, '/dist'),
     open: true,
     compress: true,
   },
